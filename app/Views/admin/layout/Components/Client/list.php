06be415f4592dd7088581f8fa69a81b9f46eb498
nguyen1608 <152713651+nguyen1608@users.noreply.github.com>
2023-12-06 00:06:44 +0700

    Update list.php

--- app/Views/admin/layout/Components/Client/list.php
+++ app/Views/admin/layout/Components/Client/list.php
@@ -3,7 +3,7 @@
 <html lang="en">
 
 <head>
-  <title>Danh Mục Khách Hàng</title>
+  <title>Danh mục Khách Hàng</title>
   <!-- Required meta tags -->
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
